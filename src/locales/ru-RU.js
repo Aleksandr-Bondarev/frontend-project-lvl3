ac589795362d25547dbc4bf578d3fdc5b5b5cfce
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-05-03 21:02:22 +0300

    updated ru locale

--- src/locales/ru-RU.js
+++ src/locales/ru-RU.js
@@ -3,5 +3,7 @@ export default {
     success: 'RSS успешно загружен',
     duplicatedURL: 'RSS уже существует',
     invalidURL: 'Ссылка должна быть валидным URL',
+    networkErr: 'Ошибка сети',
+    invalidRSS: 'Ресурс не содержит валидный RSS',
   },
 };
