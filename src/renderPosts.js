9a2d92d6533312c6c48fc0444ec13aff3cbbec62
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-05-08 18:42:29 +0300

    added logic for showing modals

--- src/renderPosts.js
+++ src/renderPosts.js
@@ -1,10 +1,34 @@
 /* global document */
+/* global bootstrap */
 /* eslint no-undef: "error" */
+
+import 'bootstrap';
+
+const linkStatusChanger = (linkId) => {
+  const targertLink = document.querySelector(`a[data-id="${linkId}"]`);
+  targertLink.classList.remove('fw-bold');
+  targertLink.classList.add('fw-normal');
+};
+
+const addContentAndShowModal = (titleContent, descriptionContent, footerLink) => {
+  const modalTitle = document.querySelector('.modal-title');
+  modalTitle.textContent = titleContent;
+
+  const modalBody = document.querySelector('.modal-body.text-break');
+  modalBody.textContent = descriptionContent;
+
+  const modalFooterLink = document.querySelector('.btn.btn-primary.full-article');
+  modalFooterLink.href = footerLink;
+
+  const modal = new bootstrap.Modal(document.querySelector('#modal'));
+  modal.show();
+};
 
 const renderList = (posts) => {
   const ul = document.createElement('ul');
-  posts.forEach((post, index) => {
-    const { title, link } = post;
+  posts.forEach((post) => {
+    const id = Math.floor(Math.random() * 100000);
+    const { title, link, description } = post;
 
     const li = document.createElement('li');
     li.classList.add(
@@ -19,7 +43,7 @@ const renderList = (posts) => {
     const a = document.createElement('a');
     a.setAttribute('href', link);
     a.classList.add('fw-bold');
-    a.setAttribute('data-id', index);
+    a.setAttribute('data-id', id);
     a.setAttribute('target', '_blank');
     a.setAttribute('rel', 'noopener noreferrer');
     a.textContent = title;
@@ -27,12 +51,13 @@ const renderList = (posts) => {
     const button = document.createElement('button');
     button.setAttribute('type', 'button');
     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-    button.setAttribute('data-id', index);
+    button.setAttribute('data-id', id);
     button.setAttribute('data-bs-toggle', 'modal');
     button.setAttribute('data-bs-target', '#modal');
     button.textContent = 'Просмотр';
     button.addEventListener('click', () => {
-      document.location = link;
+      linkStatusChanger(id);
+      addContentAndShowModal(title, description, link);
     });
 
     li.append(a, button);
